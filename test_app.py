fb3a099284aa7ce2dadc2460e8d9aa075ac9def3
Michael Mvuma <mikemvuma10@gmail.com>
2026-05-18 05:03:45 +0300

    Remove some tests to test coverage alert

--- test_app.py
+++ test_app.py
@@ -77,21 +77,21 @@ def test_add_user_missing_email_raises_error():
 #     # Naam
 #     assert False, "This test is intentionally broken"
 
-def test_add():
-    assert add(2, 3) == 5
-    assert add(-1, 1) == 0
+# def test_add():
+#     assert add(2, 3) == 5
+#     assert add(-1, 1) == 0
 
-def test_subtract():
-    assert subtract(5, 3) == 2
-    assert subtract(0, 5) == -5
+# def test_subtract():
+#     assert subtract(5, 3) == 2
+#     assert subtract(0, 5) == -5
 
-def test_multiply():
-    assert multiply(3, 4) == 12
-    assert multiply(-2, 3) == -6
+# def test_multiply():
+#     assert multiply(3, 4) == 12
+#     assert multiply(-2, 3) == -6
 
-def test_divide():
-    assert divide(10, 2) == 5.0
-    assert divide(7, 2) == 3.5
+# def test_divide():
+#     assert divide(10, 2) == 5.0
+#     assert divide(7, 2) == 3.5
 
 def test_divide_by_zero():
     with pytest.raises(ValueError):
