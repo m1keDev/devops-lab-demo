487001ea4244bb1278a1659dc85997509df78598
Michael Mvuma <mikemvuma10@gmail.com>
2026-05-21 13:35:33 +0300

    Presentation  demo 4

--- app.py
+++ app.py
@@ -25,6 +25,7 @@
 # DevOps Lab API - full data collection pipeline trigger with frontend test 12
 # DevOps Lab API - full data collection pipeline trigger with frontend test 13
 # DevOps Lab API - full data collection pipeline trigger with frontend test 14
+# DevOps Lab API - full data collection pipeline trigger with frontend test 15
 
 # DevOps Lab API - full testing 1
 from flask import Flask, jsonify
